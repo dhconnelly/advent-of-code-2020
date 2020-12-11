084fb2deda56eef3ca8233a08d39e501956bb67c
Daniel Connelly <dhconnelly@gmail.com>
2020-12-11 06:17:03 -0500

    day11 part1

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -36,6 +36,27 @@ struct Pt2 {
     col: usize,
 }
 
+impl Pt2 {
+    fn nbrs(&self) -> Vec<Pt2> {
+        let mut nbrs = Vec::new();
+        nbrs.push(Pt2 { row: self.row  , col: self.col+1 });
+        nbrs.push(Pt2 { row: self.row+1, col: self.col   });
+        nbrs.push(Pt2 { row: self.row+1, col: self.col+1 });
+        if self.row > 0 {
+            nbrs.push(Pt2 { row: self.row-1, col: self.col   });
+            nbrs.push(Pt2 { row: self.row-1, col: self.col+1 });
+        }
+        if self.col > 0 {
+            nbrs.push(Pt2 { row: self.row  , col: self.col-1 });
+            nbrs.push(Pt2 { row: self.row+1, col: self.col-1 });
+        }
+        if self.row > 0 && self.col > 0 {
+            nbrs.push(Pt2 { row: self.row-1, col: self.col-1 });
+        }
+        nbrs
+    }
+}
+
 struct Grid {
     m: HashMap<Pt2, Tile>,
     rows: usize,
@@ -53,6 +74,36 @@ impl Grid {
             }
         }
         Grid { m, rows, cols }
+    }
+
+    fn iter(&self) -> Self {
+        let m = self.m.iter().map(|(pt, tile)| {
+            let occ = pt.nbrs().iter().filter(|pt2| {
+                self.m.get(pt2) == Some(&Tile::Occupied)
+            }).count();
+            let tile2 = match (tile, occ) {
+                (Tile::Empty, 0) => Tile::Occupied,
+                (Tile::Occupied, n) if n >= 4 => Tile::Empty,
+                _ => *tile,
+            };
+            (*pt, tile2)
+        }).collect();
+        Self { m, rows: self.rows, cols: self.cols }
+    }
+
+    fn iter_until_stable<F>(&self, f: F) -> Self 
+    where F: Fn(&Self) -> Self {
+        let mut seen = std::collections::HashSet::new();
+        let mut grid = f(self);
+        loop {
+            let k = format!("{}", grid);
+            if seen.contains(&k) {
+                break;
+            }
+            seen.insert(k);
+            grid = f(&grid);
+        }
+        grid
     }
 }
 
@@ -72,5 +123,6 @@ fn main() {
     let path = std::env::args().nth(1).unwrap();
     let text = std::fs::read_to_string(&path).unwrap();
     let grid = Grid::parse(&text);
-    println!("{}", grid);
+    let stable = grid.iter_until_stable(|g| g.iter());
+    println!("{}", stable.m.values().filter(|v| **v == Tile::Occupied).count());
 }
