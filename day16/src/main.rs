0658ef7ba983698f55ec2f87a31488b9c3c47507
Daniel Connelly <dhconnelly@gmail.com>
2020-12-16 09:45:46 -0500

    day16 refactor to extract valid

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::collections::HashSet;
+
+#[derive(Debug, Clone, Copy)]
 struct Rule {
     lo1: i64,
     hi1: i64,
@@ -12,36 +14,57 @@ fn atoi(s: &str) -> i64 {
 
 fn parse_rules(s: &str) -> Vec<Rule> {
     let re = regex::Regex::new(r".+: (\d+)-(\d+) or (\d+)-(\d+)").unwrap();
-    s.lines().map(|line| {
-        let caps = re.captures(line).unwrap();
-        Rule {
-            lo1: atoi(caps.get(1).unwrap().as_str()),
-            hi1: atoi(caps.get(2).unwrap().as_str()),
-            lo2: atoi(caps.get(3).unwrap().as_str()),
-            hi2: atoi(caps.get(4).unwrap().as_str()),
-        }
-    }).collect()
+    s.lines()
+        .map(|line| {
+            let caps = re.captures(line).unwrap();
+            Rule {
+                lo1: atoi(caps.get(1).unwrap().as_str()),
+                hi1: atoi(caps.get(2).unwrap().as_str()),
+                lo2: atoi(caps.get(3).unwrap().as_str()),
+                hi2: atoi(caps.get(4).unwrap().as_str()),
+            }
+        })
+        .collect()
 }
 
-fn error_rate(rules: &[Rule], tix: &[&str]) -> i64 {
-    let mut allowed = std::collections::HashSet::new();
-    for rule in rules {
-        for i in rule.lo1..rule.hi1+1 {
-            allowed.insert(i);
-        }
-        for i in rule.lo2..rule.hi2+1 {
-            allowed.insert(i);
-        }
-    }
-    let mut rate = 0;
-    for ticket in tix {
-        for val in ticket.split(',').map(atoi) {
-            if !allowed.contains(&val) {
-                rate += val;
+fn parse_ticket(s: &str) -> Ticket {
+    s.split(',').map(atoi).collect()
+}
+
+struct Validator {
+    allowed: std::collections::HashSet<i64>,
+}
+
+type Ticket = Vec<i64>;
+
+impl Validator {
+    fn new(rules: &[Rule]) -> Self {
+        let mut allowed = HashSet::new();
+        for rule in rules {
+            for i in rule.lo1..rule.hi1 + 1 {
+                allowed.insert(i);
+            }
+            for i in rule.lo2..rule.hi2 + 1 {
+                allowed.insert(i);
             }
         }
+        Self { allowed }
     }
-    rate
+
+    fn error_rate(&self, ticket: &Ticket) -> i64 {
+        ticket.iter().filter(|n| !self.allowed.contains(n)).sum()
+    }
+
+    fn total_error_rate(&self, tix: &[Ticket]) -> i64 {
+        tix.iter().map(|ticket| self.error_rate(ticket)).sum()
+    }
+
+    fn valid_tickets(&self, tix: &[Ticket]) -> Vec<Ticket> {
+        tix.iter()
+            .filter(|ticket| self.error_rate(ticket) == 0)
+            .cloned()
+            .collect()
+    }
 }
 
 fn main() {
@@ -49,7 +72,16 @@ fn main() {
     let text = std::fs::read_to_string(&path).unwrap();
     let mut segs = text.split("\n\n");
     let rules = parse_rules(segs.next().unwrap());
-    segs.next().unwrap(); // ignore my ticket
-    let nearby: Vec<_> = segs.next().unwrap().lines().skip(1).collect();
-    println!("{}", error_rate(&rules, &nearby));
+    let mine =
+        parse_ticket(segs.next().unwrap().lines().skip(1).next().unwrap());
+    let nearby: Vec<_> = segs
+        .next()
+        .unwrap()
+        .lines()
+        .skip(1)
+        .map(parse_ticket)
+        .collect();
+
+    let validator = Validator::new(&rules);
+    println!("{}", validator.total_error_rate(&nearby));
 }
