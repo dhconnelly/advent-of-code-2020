bda593fbb5051a1b2598395b50333f21d567c71d
Daniel Connelly <dhconnelly@gmail.com>
2020-12-19 08:55:52 -0500

    day19 part1

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 fn atoi(s: &str) -> usize {
     usize::from_str_radix(s, 10).unwrap()
 }
@@ -21,23 +23,64 @@ fn parse_rule(s: &str) -> Rule {
         Rule::Char(s.chars().nth(s.len() - 2).unwrap())
     } else if s.contains('|') {
         let mut segs = s.split(" | ");
-        let alt1 = parse_seq(segs.next().unwrap());
-        let alt2 = parse_seq(segs.next().unwrap());
-        Rule::Alt(alt1, alt2)
+        let seq1 = parse_seq(segs.next().unwrap());
+        let seq2 = parse_seq(segs.next().unwrap());
+        Rule::Alt(seq1, seq2)
     } else {
         Rule::Seq(parse_seq(s))
     }
 }
 
-struct Matcher {}
+struct Matcher {
+    rules: Vec<Rule>,
+}
 
 impl Matcher {
-    fn new(rules: &[Rule]) -> Self {
-        Self {}
+    fn new(rules: Vec<Rule>) -> Self {
+        Self { rules }
     }
 
-    fn matches_all(&self, s: &str) -> bool {
-        false
+    fn match_char(&self, ch: char, s: &str, q: &mut VecDeque<usize>) -> bool {
+        match s.chars().next() {
+            Some(ch1) if ch == ch1 => self.matches_all(&s[1..], q),
+            _ => false,
+        }
+    }
+
+    fn match_seq(
+        &self,
+        seq: &[usize],
+        s: &str,
+        q: &mut VecDeque<usize>,
+    ) -> bool {
+        for r in seq.iter().rev() {
+            q.push_front(*r);
+        }
+        self.matches_all(s, q)
+    }
+
+    fn matches_all(&self, s: &str, mut q: &mut VecDeque<usize>) -> bool {
+        if q.is_empty() && s.is_empty() {
+            return true;
+        }
+        if q.is_empty() || s.is_empty() {
+            return false;
+        }
+        let first = &self.rules[q.pop_front().unwrap()];
+        match first {
+            Rule::Char(ch) => self.match_char(*ch, s, &mut q),
+            Rule::Seq(seq) => self.match_seq(&seq, s, &mut q),
+            Rule::Alt(seq1, seq2) => {
+                self.match_seq(&seq1, s, &mut q.clone())
+                    || self.match_seq(&seq2, s, &mut q.clone())
+            }
+        }
+    }
+
+    fn matches(&self, s: &str) -> bool {
+        let mut q = VecDeque::new();
+        q.push_back(0);
+        self.matches_all(s, &mut q)
     }
 }
 
@@ -48,10 +91,7 @@ fn main() {
     let mut rules_text: Vec<_> = segs[0].lines().collect();
     rules_text.sort_by_key(|s| atoi(s.split(": ").next().unwrap()));
     let rules: Vec<_> = rules_text.iter().map(|s| parse_rule(s)).collect();
-    for (i, rule) in rules.iter().enumerate() {
-        println!("rule[{}] = {:?}", i, rule);
-    }
-    let matcher = Matcher::new(&rules);
-    let valid = segs[1].lines().filter(|s| matcher.matches_all(s)).count();
+    let matcher = Matcher::new(rules);
+    let valid = segs[1].lines().filter(|s| matcher.matches(s)).count();
     println!("{}", valid);
 }
