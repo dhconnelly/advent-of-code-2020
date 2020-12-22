6c0973911f4cddf2e113a7ef5d0154b379b3101a
Daniel Connelly <dhconnelly@gmail.com>
2020-12-22 14:08:08 -0500

    day22 part2

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 
 fn atoi(s: &str) -> u64 {
@@ -20,32 +22,26 @@ fn play_round(
     p2: &mut Deck,
     mut winner: impl FnMut(&Deck, &Deck, u64, u64) -> Winner,
 ) {
-    //println!("Round");
-    //println!("Player 1's deck: {:?}", p1);
-    //println!("Player 2's deck: {:?}", p2);
     let f1 = p1.pop_front().unwrap();
-    //println!("Player 1 plays {}", f1);
     let f2 = p2.pop_front().unwrap();
-    //println!("Player 2 plays {}", f2);
     match winner(p1, p2, f1, f2) {
         Winner::Player1 => {
-            //println!("Player 1 wins the round!");
             p1.push_back(f1);
             p1.push_back(f2);
         }
         Winner::Player2 => {
-            //println!("Player 2 wins the round!");
             p2.push_back(f2);
             p2.push_back(f1);
         }
     }
 }
 
-fn score(d: &Deck) -> u64 {
+fn score(d1: &Deck, d2: &Deck) -> u64 {
+    let d = if d1.is_empty() { d2 } else { d1 };
     d.iter().rev().zip(1..).map(|(card, n)| card * n).sum()
 }
 
-fn play(mut p1: &mut Deck, mut p2: &mut Deck) -> u64 {
+fn play(mut p1: &mut Deck, mut p2: &mut Deck) -> Winner {
     while !p1.is_empty() && !p2.is_empty() {
         play_round(&mut p1, &mut p2, |_, _, f1, f2| {
             if f1 > f2 {
@@ -56,41 +52,69 @@ fn play(mut p1: &mut Deck, mut p2: &mut Deck) -> u64 {
         });
     }
     if !p1.is_empty() {
-        //println!("Winning deck: {:?}", p1);
-        score(&p1)
+        Winner::Player1
     } else {
-        //println!("Winning deck: {:?}", p2);
-        score(&p2)
+        Winner::Player2
     }
 }
 
-fn deck_string(d: &Deck) -> String {
-    d.iter()
-        .map(|x| x.to_string())
-        .collect::<Vec<_>>()
-        .join(",")
-}
-
 fn key(p1: &Deck, p2: &Deck) -> String {
-    [deck_string(p1), deck_string(p2)].join("|")
+    let mut s = String::new();
+    for card in p1 {
+        s.push_str(&card.to_string());
+        s.push(',');
+    }
+    s.push('|');
+    for card in p2 {
+        s.push_str(&card.to_string());
+        s.push(',');
+    }
+    s
 }
 
+#[derive(Clone, Copy)]
 enum Winner {
     Player1,
     Player2,
 }
 
-fn play_recursive(mut p1: &mut Deck, mut p2: &mut Deck) -> (Winner, u64) {
-    let mut seen = std::collections::HashSet::new();
+fn play_recursive(
+    mut p1: &mut Deck,
+    mut p2: &mut Deck,
+    //mut memo: &mut HashMap<String, Winner>,
+) -> Winner {
+    //println!("{:?} {:?}", p1, p2);
+    let mut seen = HashSet::new();
     while !p1.is_empty() && !p2.is_empty() {
         let k = key(p1, p2);
         if seen.contains(&k) {
-            return (Winner::Player1, score(&p1));
+            return Winner::Player1;
         }
         seen.insert(k);
         play_round(&mut p1, &mut p2, |p1, p2, f1, f2| {
             if p1.len() as u64 >= f1 && p2.len() as u64 >= f2 {
-                play_recursive(&mut p1.clone(), &mut p2.clone()).0
+                //let k1 = key(p1, p2);
+                //if let Some(result) = memo.get(&k1) {
+                ////println!("cache hit: {}", k1);
+                //*result
+                //} else {
+                let result = play_recursive(
+                    &mut p1
+                        .clone()
+                        .iter()
+                        .take(f1 as usize)
+                        .copied()
+                        .collect(),
+                    &mut p2
+                        .clone()
+                        .iter()
+                        .take(f2 as usize)
+                        .copied()
+                        .collect(), //&mut memo,
+                );
+                //memo.insert(k1, result);
+                result
+            //}
             } else if f1 > f2 {
                 Winner::Player1
             } else {
@@ -99,18 +123,22 @@ fn play_recursive(mut p1: &mut Deck, mut p2: &mut Deck) -> (Winner, u64) {
         });
     }
     if !p1.is_empty() {
-        //println!("Winning deck: {:?}", p1);
-        (Winner::Player1, score(&p1))
+        Winner::Player1
     } else {
-        //println!("Winning deck: {:?}", p2);
-        (Winner::Player2, score(&p2))
+        Winner::Player2
     }
 }
 
 fn main() {
     let path = std::env::args().nth(1).unwrap();
     let text = std::fs::read_to_string(&path).unwrap();
-    let (p1, p2) = read_decks(&text);
-    println!("{}", play(&mut p1.clone(), &mut p2.clone()));
-    println!("{}", play_recursive(&mut p1.clone(), &mut p2.clone()).1);
+    let (deck1, deck2) = read_decks(&text);
+
+    //let (mut p1, mut p2) = (deck1.clone(), deck2.clone());
+    //play(&mut p1, &mut p2);
+    //println!("{}", score(&p1, &p2));
+
+    let (mut p1, mut p2) = (deck1.clone(), deck2.clone());
+    play_recursive(&mut p1, &mut p2 /*&mut HashMap::new()*/);
+    println!("{}", score(&p1, &p2));
 }
