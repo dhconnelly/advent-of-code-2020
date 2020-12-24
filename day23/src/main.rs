f00942d458972ec1f74da4c3ffa7acfe4c2370c5
Daniel Connelly <dhconnelly@gmail.com>
2020-12-23 21:43:24 -0500

    day23: part2, switch to a linked list using a vec-based successor list since rust's std::collection::linkedlist api is almost useless

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -18,55 +18,55 @@ fn destination(cur: usize, not: &[usize], m: usize) -> usize {
     dest
 }
 
-fn to_remove(v: &[usize], n: usize, from: usize) -> Vec<usize> {
-    (from..from + n).map(|i| v[i % v.len()]).collect()
+fn play(a: &mut Vec<usize>, cur: usize) -> usize {
+    let r1 = a[cur - 1];
+    let r2 = a[r1 - 1];
+    let r3 = a[r2 - 1];
+    let dst = destination(cur, &[r1, r2, r3], a.len());
+    let prev_next = a[dst - 1];
+    a[cur - 1] = a[r3 - 1];
+    a[dst - 1] = r1;
+    a[r3 - 1] = prev_next;
+    a[cur - 1]
 }
 
-fn play(v: &mut Vec<usize>, cur_index: usize) -> usize {
-    let cur = v[cur_index];
-    let remove = to_remove(v, 3, cur_index + 1);
-    let dest = destination(cur, &remove, v.len());
-    let dest_index = v.iter().position(|x| *x == dest).unwrap();
-
-    let mut tmp = Vec::with_capacity(v.len());
-    tmp.push(dest);
-    for cup in &remove {
-        tmp.push(*cup);
+fn to_successor_list(v: &[usize]) -> Vec<usize> {
+    let mut a = vec![0; v.len()];
+    for i in 0..v.len() {
+        a[v[i] - 1] = v[(i + 1) % v.len()];
     }
-    let mut new_cur_index = 0;
-    for k in 0..v.len() - 1 {
-        let i = (dest_index + k + 1) % v.len();
-        if remove.contains(&v[i]) {
-            // do this faster? it's always just three elements though
-            continue;
-        }
-        if v[i] == v[cur_index] {
-            new_cur_index = tmp.len();
-        }
-        tmp.push(v[i]);
-    }
-    *v = tmp;
-    (new_cur_index + 1) % v.len()
+    a
 }
 
 fn play_n(v: &[usize], n: usize) -> Vec<usize> {
-    let mut v = v.iter().copied().collect();
-    let mut start = 0;
+    let mut a = to_successor_list(v);
+    let mut cur = v[0];
     for _ in 0..n {
-        start = play(&mut v, start);
+        cur = play(&mut a, cur);
     }
-    v
+    a
 }
 
 fn main() {
     let path = std::env::args().nth(1).unwrap();
     let text = std::fs::read_to_string(&path).unwrap();
     let v: Vec<_> = text.lines().next().unwrap().chars().map(atoi).collect();
-    let v = play_n(&v, 100);
 
-    let pos1 = v.iter().position(|x| *x == 1).unwrap();
-    let from1: Vec<_> =
-        v.iter().cycle().skip(pos1 + 1).take(v.len() - 1).collect();
-    let str1 = from1.iter().map(|x| x.to_string()).collect::<String>();
-    println!("{}", str1);
+    let a1 = play_n(&v, 100);
+    let mut s = String::new();
+    let mut last = 1;
+    while s.len() < v.len() - 1 {
+        s.push_str(&a1[last - 1].to_string());
+        last = a1[last - 1];
+    }
+    println!("{}", s);
+
+    let a2 = play_n(
+        &v.iter()
+            .copied()
+            .chain(v.len() + 1..=1000000)
+            .collect::<Vec<_>>(),
+        10000000,
+    );
+    println!("{}", a2[0] * a2[a2[0] - 1]);
 }
